695b47db9d8b7d1da8f3b74f890aa8a70d1535cc
JonKoala <victor.santana.lemos@gmail.com>
2018-04-26 18:41:15 -0300

    aplica logica de async/await nos testes

--- test/queries.js
+++ test/queries.js
@@ -1,11 +1,11 @@
-var chai = require('chai');
+var chai = require('chai')
 
 var expect = chai.expect;
 chai.use(require('chai-http'));
 chai.use(require('chai-things'));
 
-var server = require('../server');
-var query = require('../dbi/query');
+var server = require('../server')
+var query = require('../dbi/query')
 
 
 describe('queries route', function() {
@@ -16,100 +16,75 @@ describe('queries route', function() {
 
   describe('/GET requests', function() {
 
-    it('should get an empty array if no query exists', function() {
-      return chai.request(server)
-        .get('/queries')
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('array');
+    it('should get an empty array if no query exists', async function() {
+      var response = await chai.request(server).get('/queries')
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.an('array');
 
-          expect(res.body).to.have.lengthOf(0);
-        });
+      expect(response.body).to.have.lengthOf(0);
     });
 
-    it('should get an array of queries if at least one query exists', function() {
-      query.create({titulo: 'query 1', corpo: 'select 1'})
-      return chai.request(server)
-        .get('/queries')
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('array');
+    it('should get an array of queries if at least one query exists', async function() {
+      await query.create({titulo: 'query 1', corpo: 'select 1'});
 
-          expect(res.body).to.have.lengthOf.at.least(1);
-          expect(res.body).all.to.have.keys('id', 'titulo', 'corpo');
-        });
+      var response = await chai.request(server).get('/queries');
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.an('array');
+
+      expect(response.body).to.have.lengthOf.at.least(1);
+      expect(response.body).all.to.have.keys('id', 'titulo', 'corpo');
     });
 
-    it('should get an empty object if specified id doesn\'t exist', function() {
-      return chai.request(server)
-        .get('/queries/1')
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.null;
-        });
+    it('should get null as response if specified id doesn\'t exist', async function() {
+      var response = await chai.request(server).get('/queries/1');
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.null;
     });
 
-    it('should get a query object if specified id exists', function() {
-      query.create({titulo: 'query 1', corpo: 'select 1'})
-      return chai.request(server)
-        .get('/queries/1')
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('object');
+    it('should get a query object if specified id exists', async function() {
+      await query.create({titulo: 'query 1', corpo: 'select 1'});
 
-          expect(res.body).to.have.keys('id', 'titulo', 'corpo');
-        });
+      var response = await chai.request(server).get('/queries/1');
+      expect(response).to.have.status(200);
+      expect(response.body).to.be.an('object');
+
+      expect(response.body).to.have.keys('id', 'titulo', 'corpo');
     });
 
   });
 
   describe('/POST requests', function() {
 
-    it('should persist a valid query object', function() {
-      var newQuery = {titulo: 'query 1', corpo: 'select 1'}
-      return chai.request(server)
-        .post('/queries')
-        .send(newQuery)
-        .then(function(res) {
-          expect(res).to.have.status(200);
+    it('should persist a valid query object', async function() {
+      var newQuery = {titulo: 'query 1', corpo: 'select 1'};
 
-          return query.findById(1, {raw:true});
-        }).then(function(result) {
+      var serverResponse = await chai.request(server).post('/queries').send(newQuery);
+      expect(serverResponse).to.have.status(200);
 
-          expect(result).to.deep.equal(Object.assign({id: 1}, newQuery));
-        });
+      var expectedDatabaseSelectResult = Object.assign({id: 1}, newQuery);
+      var databaseSelectResult = await query.findById(1, {raw:true});
+      expect(databaseSelectResult).to.deep.equal(expectedDatabaseSelectResult);
     });
 
-    it('shouldn\'t persist an invalid query object', function() {
-      var newQuery = {titulo: 'query 1'}
-      return chai.request(server)
-        .post('/queries')
-        .send(newQuery)
-        .then(function(res) {
-          expect(res).to.have.status(500);
+    it('shouldn\'t persist an invalid query object', async function() {
+      var newInvalidQuery = {titulo: 'query 1'};
 
-          return query.findById(1, {raw:true});
-        }).then(function(result) {
-          expect(result).to.be.null;
-        });
+      var serverResponse = await chai.request(server).post('/queries').send(newInvalidQuery);
+      expect(serverResponse).to.have.status(500);
+
+      var databaseSelectResult = await query.findById(1, {raw:true});
+      expect(databaseSelectResult).to.be.null;
     });
 
-    it('should return the persisted query object', function() {
-      var newQuery = {titulo: 'query 1', corpo: 'select 1'}
-      return chai.request(server)
-        .post('/queries')
-        .send(newQuery)
-        .then(function(res) {
-          expect(res).to.have.status(200);
-          expect(res.body).to.be.an('object');
+    it('should return the persisted query object', async function() {
+      var newQuery = {titulo: 'query 1', corpo: 'select 1'};
 
-          return Promise.all([res.body, query.findById(1, {raw:true})]);
-        }).then(function(results) {
-          var objectReturnedFromServer = results[0]
-          var objectFoundInDatabase = results[1]
+      var serverResponse = await chai.request(server).post('/queries').send(newQuery);
+      expect(serverResponse).to.have.status(200);
+      expect(serverResponse.body).to.be.an('object');
 
-          expect(objectReturnedFromServer).to.deep.equal(objectFoundInDatabase);
-        });
+      var databaseSelectResult = await query.findById(1, {raw:true});
+      expect(databaseSelectResult).to.deep.equal(serverResponse.body);
     });
 
   });
